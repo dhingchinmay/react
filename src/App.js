ca94137d3a53a53b0b35e5b708f566307df57a55
ChinmayDhing <chinmaydhi123@gmail.com>
2021-09-28 17:52:34 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import ExpenseItem from "./ExpenseItem.js/ExpenseItem";
+import ExpenseItem from "./Expenses.js/ExpenseItem";
 
 function App() {
     const expenses = [{
@@ -24,36 +24,25 @@ function App() {
     ];
 
     return ( <
-        div className = "App" >
+        div >
         <
-        header className = "App-header" >
-        <
-        p >
-        <
+        h2 > Let 's get started!</h2> <
         ExpenseItem title = { expenses[0].title }
         amount = { expenses[0].amount }
-        date = { expenses[0].date } >
-        < /ExpenseItem> <
+        date = { expenses[0].date }
+        /> <
         ExpenseItem title = { expenses[1].title }
         amount = { expenses[1].amount }
-        date = { expenses[1].date } >
-        < /ExpenseItem> <
+        date = { expenses[1].date }
+        /> <
         ExpenseItem title = { expenses[2].title }
         amount = { expenses[2].amount }
-        date = { expenses[2].date } >
-        < /ExpenseItem> <
+        date = { expenses[2].date }
+        /> <
         ExpenseItem title = { expenses[3].title }
         amount = { expenses[3].amount }
-        date = { expenses[3].date } >
-        < /ExpenseItem> <
-        /p> <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React <
-        /a> <
-        /header> <
+        date = { expenses[3].date }
+        /> <
         /div>
     );
 }
